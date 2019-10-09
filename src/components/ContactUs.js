a0338fc67239541e78a588db1859368ad26dccdd
Xavier Mas <xavier@relevanzz.com>
2019-10-09 10:35:53 +0200

    second commit

---
+++ src/components/ContactUs.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import ReactContactForm from "react-mail-form";
+
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+        <div className="row section-head">
+          <div className="ten columns">
+            <h3 className="lead">
+              Feel free to contact me for any work or suggestions below
+            </h3>
+          </div>
+        </div>
+        {/*<div className="row">
+          <aside className="eigth columns footer-widgets">
+            <div className="widget">
+              <h4>Linked in :{resumeData.linkedinId}</h4>
+            </div>
+          </aside>
+    </div>*/}
+        <div className="row">
+          <ReactContactForm to="xavier.mas.le@gmail.com" buttonText="Send"/>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,139 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Xavier Mas Leszkiewicz",
+  role: "Entrepeneur, Sotware Engineer and Teacher",
+  linkedinId: "xavier-mas-leszkiewicz",
+  skypeid: "xmasle.telecos",
+  roleDescription:
+    "A curious person who looks forward to learning things every day. I am really passionate about technology, teaching, and coaching.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/xavier-mas-leszkiewicz/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://github.com/masosky",
+      className: "fa fa-github"
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/xmasle",
+      className: "fa fa-twitter"
+    }
+  ],
+  aboutme: "Co-Founder and CTO at Relevanzz.",
+  currentJob: `At Relevanzz we are solving the problem of e-mail marketing: Nobody reads promotional emails.
+
+    We are building software for e-commerce companies to take their email marketing strategies into Social Ads. Our goal is to help companies to create more intimate relations with their customers, increasing brand awareness, retention and advocacy.
+    
+    Our test customers have increased their reach from 6% to 70% and multiplied the revenues derived from their campaigns up to 4 times.`,
+  address: "Barcelona",
+  website: "https://relevanzz.com",
+  education: [
+    {
+      UniversityName:
+        "UPC School. School of Professionals & Executive Development",
+      specialization: "Big Data Management & Analytics",
+      MonthOfPassing: "July",
+      YearOfPassing: "2015"
+      // Achievements: "Some Achievements"
+    },
+    {
+      UniversityName: "UPC. Universitat Politècnica de Catalunya",
+      specialization: "Bachelor degree in Audiovisual Systems",
+      MonthOfPassing: "March",
+      YearOfPassing: "2011"
+      // Achievements: "Some Achievements"
+    },
+    {
+      UniversityName: "Technische Universität Berlin",
+      specialization: "Bachelor Thesis",
+      MonthOfPassing: "February",
+      YearOfPassing: "2011",
+      Achievements:
+        "Object Tracking with multiple cameras using real world coordinates"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "eDreams ODIGEO",
+      specialization: "Software Engineer - Machine Learning POD",
+      MonthOfLeaving: "July",
+      YearOfLeaving: "2019",
+      Achievements: `Working as a senior software engineer at Machine Learning Team at eDreams Odigeo.
+      My goal is to design and provide a robust back-end infrastructure capable to serve different Machine Learning technologies.
+      I am working really close to Data Science and providing the latest technology to my company.
+      Basically working with Java, Python, Docker, Maven, Kafka, Kuberntes, Kibana, Elastic Search and related technologies.`
+    },
+    {
+      CompanyName: "eDreams ODIGEO",
+      specialization: "Java Developer - BI Revamp",
+      MonthOfLeaving: "February",
+      YearOfLeaving: "2018",
+      Achievements: `Working with Java and all related technologies as NOSQL, SQL, Maven, and other technologies. 
+      My goal was to treat a big amount of data, treat this data and finally store it.
+      I had complete ownership with this project, since designing until deploying to production.`
+    },
+    {
+      CompanyName: "SIA Group. Sistemas Informáticos Abiertos",
+      specialization: "Java Developer and IT Security",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2017",
+      Achievements: `Working in a government environment involving a wide variety of technologies and different operating systems. Managing more than 250,000 identities and Sign-On protection for these identities.
+      Responsible for migration of many applications and responsible for improving or creating scripts using Oracle Identity Manager
+      Mostly focused on Java applications and similar. Dealing with many technologies as Active Directory, Oracle database, Visual Basic, Oracle Identity Manager, Siteminder (SSO) and frameworks as Spring or Struts.
+      `
+    }
+  ],
+  skillsDescription: "My most outstanding skills",
+  skills: [
+    {
+      skillname: "Software Engineering"
+    },
+    {
+      skillname: "People Oriented"
+    },
+    {
+      skillname: "Motivator"
+    },
+    {
+      skillname: "Amiable profile"
+    }
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg"
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png"
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    }
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    }
+  ]
+};
+
+export default resumeData;
