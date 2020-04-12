f2e74aeeaa8e08c9a393bc7ffcec7580d9e4bfe3
Xavier Mas <xavier@relevanzz.com>
2020-04-12 19:23:17 +0200

    google-analytics fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 //import ReactDOM from 'react-dom';
-import './index.css';
-import App from '../components/App';
+import "./index.css";
+import App from "../components/App";
+import ReactGA from "react-ga";
 //import * as serviceWorker from '../serviceWorker';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-export default () => <App/>
+export default () => {
+  ReactGA.initialize("UA-110555374-1");
+  return <App />;
+};
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
