fe283457e1f260341f7c84d75255228f0cbfccb0
Xavier Mas Leszkiewicz <xavier.mas.le@gmail.com>
2021-02-11 19:14:46 +0100

    Profile pic

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Header from "./Header";
 import About from "./About";
 import Resume from "./Resume";
@@ -9,47 +9,56 @@ import ContactUs from "./ContactUs";
 import Footer from "./Footer";
 import resumeData from "../resumeData";
 import ReactGA from "react-ga";
-import { Helmet } from "react-helmet";
-import { withPrefix } from "gatsby";
+import {Helmet} from "react-helmet";
+import {withPrefix} from "gatsby";
+import MetaTags from 'react-meta-tags';
 
 class App extends Component {
-  componentDidMount() {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }
-  render() {
-    return (
-      <div className="App">
-        <Helmet>
-          <meta charset="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <link rel="shortcut icon" href="favicon.ico" />
-          <meta name="note" content="environment=development" />
-          <meta name="theme-color" content="#000000" />
+    componentDidMount() {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }
 
-          <link rel="manifest" href="manifest.json" />
+    render() {
+        return (
+            <div className="App">
+                <Helmet>
+                    <meta charset="utf-8"/>
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
+                    <link rel="shortcut icon" href="favicon.ico"/>
+                    <meta name="note" content="environment=development"/>
+                    <meta name="theme-color" content="#000000"/>
 
-          <title>Xavier Mas Leszkiewicz</title>
-          <link rel="stylesheet" href="css/default.css" />
-          <link rel="stylesheet" href="css/layout.css" />
-          <link rel="stylesheet" href="css/media-queries.css" />
-          <link rel="stylesheet" href="css/magnific-popup.css" />
+                    <link rel="manifest" href="manifest.json"/>
 
-          <script src={withPrefix("script.js")} type="text/javascript" />
-        </Helmet>
-        <Header resumeData={resumeData} />
-        <About resumeData={resumeData} />
-        <Resume resumeData={resumeData} />
-        <Music resumeData={resumeData} />
-        {/*<Portfolio resumeData={resumeData}/>*/}
-        {/*<Testimonials resumeData={resumeData}/>*/}
-        <ContactUs resumeData={resumeData} />
-        <Footer resumeData={resumeData} />
-      </div>
-    );
-  }
+                    <title>Xavier Mas Leszkiewicz</title>
+                    <MetaTags>
+                        <title>Page 1</title>
+                        <meta name="description" content="Xavier's Personal Website."/>
+                        <meta property="og:title" content="Xavier's Personal Website."/>
+                        <meta property="og:image" content="images/profilepic3.jpg"/>
+                    </MetaTags>
+                    <meta name="description" content="Xavier's Personal Website"/>
+                    <link rel="stylesheet" href="css/default.css"/>
+                    <link rel="stylesheet" href="css/layout.css"/>
+                    <link rel="stylesheet" href="css/media-queries.css"/>
+                    <link rel="stylesheet" href="css/magnific-popup.css"/>
+
+                    <script src={withPrefix("script.js")} type="text/javascript"/>
+                </Helmet>
+                <Header resumeData={resumeData}/>
+                <About resumeData={resumeData}/>
+                <Resume resumeData={resumeData}/>
+                <Music resumeData={resumeData}/>
+                {/*<Portfolio resumeData={resumeData}/>*/}
+                {/*<Testimonials resumeData={resumeData}/>*/}
+                <ContactUs resumeData={resumeData}/>
+                <Footer resumeData={resumeData}/>
+            </div>
+        );
+    }
 }
 
 export default App;
